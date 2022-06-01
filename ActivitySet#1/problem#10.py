f335d7e4898d5f1962e0d56feb1e23ccb649e9f9
Sahanna Rachel <96898244+sahannarachel@users.noreply.github.com>
2022-06-01 03:31:12 +0000

    9

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,17 @@
 # Dictionaries
-
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+fh=open(name)
+words=list()
+counts =dict()
+for line in fh:
+    if line.startswith("From:"):
+        words=line.split()
+        if words[1] not in counts:
+            counts[words[1]]=1
+        else:
+            counts[words[1]]+=1
+you=counts.values()
+z=max(you)
+for key in counts:
+    if counts[key]==z:
+        print(key,z)
