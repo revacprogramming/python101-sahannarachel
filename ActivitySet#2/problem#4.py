9d76206fb82356537bf7e237fe98a188fd21d1aa
sahannarachel <96898244+sahannarachel@users.noreply.github.com>
2022-06-29 10:18:41 +0530

    Update problem#4.py

--- ActivitySet#2/problem#4.py
+++ ActivitySet#2/problem#4.py
@@ -1,26 +1,41 @@
-
-
+#"""get string input"""
 def get_cs():
-    """get string input"""
+    str=input('enter the shortcuts and its reps in form "word=sf" \n')
+    return str
 
+#"""convert connected string to list of strings"""
+def cs_to_lot(cs,lst):
+    word=cs.split(';')
+    
+    for exam in word:
+      short=exam.split('=')
+      tups=(short[0],short[1])
+      lst.append(tups)
+    #for test in word:
+        #lst.append(test)
+    return lst
 
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
+#"""convert list of strings to connected string"""
 def lot_to_cs(lot):
-    """convert list of strings to connected string"""
-
+    fc=list()
+    fest=None
+    for truce in lot:
+        tr=truce[0]+'='+truce[1]
+        fc.append(tr)
+        for trick in fc:
+            if fest==None:
+                fest=trick
+            else:
+                fest=fest+trick
+        return fest
 
 def main():
-    cs=get_cs()
-
-    lot=cs_to_lot(cs)  # convert connect string to list of tuples
+    cs = get_cs()
+    lot=list()
+    lot = cs_to_lot(cs,lot)
     print(lot)
-
-    cs=lot_to_cs(lot)  # convert list of strings to connect string
+    #out=dict()
+    #cs=lot_to_cs(lot) 
+    #print(out.items())
+    cs=lot_to_cs(lot)
     print(cs)
-
-
-if __name__ == '__main__':
-    main()
