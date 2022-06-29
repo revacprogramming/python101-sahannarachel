e6b25524a0417f2a1dbeced833d305367afb50b9
sahannarachel <96898244+sahannarachel@users.noreply.github.com>
2022-06-29 10:17:20 +0530

    Update problem#3.py

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,26 @@
-
-
+#"""get string input"""
 def get_cs():
-    """get string input"""
+    str=input('enter the shortcuts and its reps in form "word=sf" \n')
+    return str
 
-
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
+#"""convert connected string to list of strings"""
+def cs_to_lot(cs,lst):
+    word=cs.split(';')
+    for test in word:
+        lst.append(test)
+    return lst
 
 def main():
     cs = get_cs()
-
-    lot = cs_to_lot(cs)
+    lot=list()
+    lot = cs_to_lot(cs,lot)
+    #print(lot)
+    #out=dict()
+    #for test in lot:
+     #   short=test.split('=')
+      #  out[short[0]] = short[1]
     print(lot)
 
-
-if __name__ == '__main__':
+process=input('what process r u doing? \n')
+if process == 'strsplit':
     main()
