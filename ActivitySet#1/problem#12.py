511643f5e69504929b700a29cf14a7c580d93c44
Sahanna Rachel <96898244+sahannarachel@users.noreply.github.com>
2022-07-06 12:41:24 +0000

    done

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,3 +1,14 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
-int main
+import re
+name = input("Enter file:")
+fh=open(name)
+sum=0
+lst=list()
+for line in fh:
+    line=line.rstrip()
+    y=re.findall('[0-9]+',line)
+    lst=lst+y
+for i in lst:
+    sum=sum+int(i)
+print(sum)
