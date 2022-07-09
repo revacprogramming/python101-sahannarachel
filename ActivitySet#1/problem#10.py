8b50b94d80cfdb605f3b436829ef1c12d095dd2e
Sahanna Rachel <96898244+sahannarachel@users.noreply.github.com>
2022-07-09 05:57:51 +0000

    done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,19 +1,18 @@
 # Dictionaries
 name = input("Enter file:")
-fh=open(name)
+handle = open(name)
 words=list()
-counts =dict()
-for line in fh:
-    if line.startswith("From:"):
-        words=line.split()
-        if words[1] not in counts:
-            counts[words[1]]=1
-        else:
-            counts[words[1]]+=1
-you=counts.values()
-z=max(you)
+counts=dict()
+for line in handle:
+    line=line.rstrip()
+    if not line.startswith('From:'):        continue
+    words=line.split()
+    if words[1] not in counts:
+        counts[words[1]]=1
+    else:
+        counts[words[1]]+=1
+x=counts.values()
+z=max(x)
 for key in counts:
     if counts[key]==z:
         print(key,z)
-      
-      
