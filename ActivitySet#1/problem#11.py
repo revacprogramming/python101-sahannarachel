99e44a98887076afb3155692cc146fbbb859da4b
Sahanna Rachel <96898244+sahannarachel@users.noreply.github.com>
2022-07-10 06:23:06 +0000

    done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,5 +1,20 @@
 # Tuples
-
 filename = "dataset/mbox-short.txt"
 
-
+name = input("Enter file:")
+handle = open(name)
+counts=dict()
+lst=list()
+for line in handle:
+    line=line.rstrip()
+    if not line.startswith('From '):        continue
+    words = line.split()
+    x=words[5]
+    y=x[ :2]
+    lst.append(y)
+lst.sort()
+for x in lst:
+    counts[x]=counts.get(x,0)+1
+sorted(counts.items())
+for key, val in sorted(counts.items()):
+    print(key,val)
